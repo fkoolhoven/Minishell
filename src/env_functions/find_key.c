5edf8df081c3ab33c514bbad618f93ffba82297b
Jagna Hendriks <jhendrik@f0r5s15.codam.nl>
2023-09-22 15:06:20 +0200

    added better find key function

--- src/env_functions/find_key.c
+++ src/env_functions/find_key.c
@@ -6,7 +6,7 @@
 /*   By: jhendrik <marvin@42.fr>                     +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2023/09/20 16:53:05 by jhendrik      #+#    #+#                 */
-/*   Updated: 2023/09/22 14:42:37 by jhendrik      ########   odam.nl         */
+/*   Updated: 2023/09/22 15:05:43 by jhendrik      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -20,13 +20,67 @@ static char	*st_search_key(t_hnode *list, char *key)
 	tmp = list;
 	while (tmp)
 	{
-		if (ft_strncmp(key + 1, tmp->key, ft_strlen(key) - 1) == 0)
+		if (ft_strncmp(key, tmp->key, ft_strlen(key)) == 0)
 			return (tmp->value);
 		tmp = tmp->next;
 	}
 	return (NULL);
 }
 
+static char	*st_find_newkey(t_htable *env, char *key)
+{
+	int		index;
+	char	*rtn;
+	char	*new_key;
+
+	new_key = ft_strdup(key + 1);
+	if (new_key != NULL)
+	{
+		index = give_hash_index(new_key, env);
+		if (index < 0)
+			return (NULL);
+		rtn = st_search_key((env->array)[index], new_key);
+		free(new_key);
+		return (rtn);
+	}
+	else
+		return (NULL);
+}
+
+char	*find_env_value(t_htable *env, char *key)
+{
+	int		index;
+	char	*rtn;
+
+	if (env == NULL || key == NULL)
+		return (NULL);
+	if (env->array == NULL)
+		return (NULL);
+	if (key[0] == '$')
+		return (st_find_newkey(env, key));
+	else
+	{
+		index = give_hash_index(key, env);
+		if (index < 0)
+			return (NULL);
+		return (st_search_key((env->array)[index], key));
+	}
+}
+static t_hnode	*st_search_node(t_hnode *list, char *key)
+{
+	t_hnode	*tmp;
+
+	if (list == NULL || key == NULL)
+		return (NULL);
+	tmp = list;
+	while (tmp)
+	{
+		if (ft_strncmp(key, tmp->key, ft_strlen(key)) == 0)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
 char	*find_env_value(t_htable *env, char *key)
 {
 	int		index;
