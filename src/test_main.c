c5c05884c851c7ddf5e6bf50abc61c9fa007c362
Jagna Hendriks <jhendrik@f0r5s15.codam.nl>
2023-09-22 15:01:29 +0200

    changed testmain

--- src/test_main.c
+++ src/test_main.c
@@ -6,7 +6,7 @@
 /*   By: jhendrik <marvin@42.fr>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/05 16:54:34 by jhendrik      #+#    #+#                 */
-/*   Updated: 2023/09/22 12:53:26 by jhendrik      ########   odam.nl         */
+/*   Updated: 2023/09/22 14:51:10 by jhendrik      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -14,19 +14,19 @@
 int	main(int argc, char **argv, char **envp)
 {
 	t_htable	*env;
-//	char		*key;
-//	char		*value;
-//	char		*found;
+	char		*key;
+	char		*value;
+	char		*found;
 	char		**new_env;
-//	t_hnode		*node;
-	int			check;
+	t_hnode		*node;
+//	int			check;
 
 	if (argc < 1 || argv == NULL)
 		return (EXIT_FAILURE);
 	printf("HELLO I'm in main\n");
 	print_strarray(envp);
 	env = init_env(envp);
-/*	delete_pair(env, "LS_COLOR");
+	delete_pair(env, "LS_COLOR");
 	delete_pair(env, "DEFAULTS_PATH");
 	key = ft_strdup("CATS");
 	value = ft_strdup("Lee:Andora:Gato:Sniffels:John:Guusje:");
@@ -40,23 +40,23 @@ int	main(int argc, char **argv, char **envp)
 	print_hashnode(node, env);
 	node = find_env_valuenode(env, "NONESENSE");
 	print_hashnode(node, env);
-	found = find_env_value(env, "CATS");
+	found = find_env_value(env, "$CATS");
 	printf("Found value: %s\n", found);
 	found = find_env_value(env, "NONESENSE");
 	printf("Found value: %s\n", found);
 	found = find_env_value(env, NULL);
 	printf("Found value: %s\n", found);
-	found = find_env_value(env, "PATH");
-	printf("Found value: %s\n", found); */
+	found = find_env_value(env, "$PATH");
+	printf("Found value: %s\n", found); 
 	new_env = convert_htable_to_strarray(env);
-	print_strarray(new_env);
-	check = comparing_envs(envp, new_env);
+//	print_strarray(new_env);
+/*	check = comparing_envs(envp, new_env);
 	if (check == 0)
 		printf("The environments are the same\n");
 	else if (check == 1)
 		printf("The new environment is missing an element\n");
 	else 
-		printf("One of the environments is NULL\n");
+		printf("One of the environments is NULL\n"); */
 	ft_free_str_array(new_env);
 	terminate_hashtable(env);
 	return (0);
